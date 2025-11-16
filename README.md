eca222498bcc7c369169a3f8a2fb4df2780f623b
asus <joaocabralpv@gmail.com>
2025-11-16 17:27:37 -0100

    Make algorithm actualy work

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Factorization-Algorithm-for-affine-reconstruction
-Implementation of the Factorization Algorithm described in the paper "Shape and motion from image streams under orthography: A
-factorization approach" by C. Tomasi and T. Kanade
+Implementation of the Factorization Algorithm for affine reconstruction from parallel projection as described in Multiple View Geometry in Computer Vision.
+
+This algorithm was firt discribed in the paper "Shape and motion from image streams under orthography: A factorization approach" by C. Tomasi and T. Kanade
 
 # How to use
-On the affineReconstruction.py file, put the points' coordinates in the image from each camera in the c1,c2,c3 lists. Corresponding points should have the same index on the different camera lists. The algorithm also works with a different number of cameras; Make a list with the points of that camera and then add that list to the list a.
+On the affineReconstruction.py file, put the points' coordinates in the image from each camera in the `c1`,`c2`,`c3` arrays. Corresponding points should have the same index on different  lists. The algorithm also works with a different number of views; Make an array with the points of that view and then add that list to the list `cameras`.
+
+The script will print the coordinates of the points in 3D space in the order that they were put in the camara arrays and the the inhomogineous camera matricies and translations correspoinding to each view in the order that they were put in `cameras`
--- affineReconstruction.py
+++ affineReconstruction.py
@@ -1,88 +1,105 @@
 import numpy as np
+from sys import exit
 
-c1=[
-[812,505],
-[812,546],
-[850,402],
-[850,444],
-[601,490],
-[601,531],
-]
+# create arrays with the images of the points for each camera
+# they have to be np arrays
+c1=np.array([
+[969,526],
+[969,467],
+[658.0,582.0],
+[1088.0,648.0],
+[708.0,593.0],
+[1089.0,590.0]
+])
 
-c2=[
-[530,438],
-[530,454],
-[582,220],
-[582,236],
-[317,394],
-[317,410],
-]
+c2=np.array([
+[458,669],
+[457,608],
+[543.0,785.0],
+[754.0,641.0],
+[571.0,768.0],
+[754.0,581.0]
+])
 
-c3=[
-[549,394],
-[549,456],
-[873,393],
-[874,456],
-[542,370],
-[542,434],
-]
-
-a=[c1,c2,c3]
-""""
-for i in range(3):
-    for j in range(len(a[i])):
-       a[i][j][1]=1031-a[i][j][1]
-       #print(f"{a[i][j][0]},{a[i][j][1]}") 
-"""
-c=[]
-
-for i in range(len(a)):
-    x=0
-    y=0
-    for j in range(len(a[i])):
-        x+=a[i][j][0]
-        y+=a[i][j][1]
-    x=x/len(a[i])
-    y=y/len(a[i])
-    c.append([x,y])
-    #print(f"centroid{i+1}=[{x},{y}]")
-
-for i in range(len(a)):
-    for j in range(len(a[i])):
-        a[i][j][0]-=c[i][0]
-        a[i][j][1]-=c[i][1]
-    #print(f"c{i+1}={a[i]}")
+c3=np.array([
+[712,455],
+[712,394],
+[533.0,554.0],
+[972.0,512.0],
+[586.0,552.0],
+[972.0,451.0]
+])
 
 
-W=np.empty((len(a)*2,len(c1)))
+#put the arrays that you made before in this list (don't change the name)
+cameras=[c1,c2,c3]
 
-for i in range(len(a)):
-    for j in range(len(c1)):
-        W[2*i,j]=a[i][j][0]
-        b=a[i][j][1]
-        W[(2*i)+1,j]=b
+# check if each point has two coordinates
+n_points=-1
+for camera in cameras:
+    for point in camera:
+        print(point)
+        if len(point) != 2:
+            print("Error, each point should have 2 coordinates")
+            exit(1)
+    if n_points==-1:
+        n_points=len(camera)
+    elif n_points != len(camera):
+        print("Each camera should have the same number of points")
+        print(camera)
+        exit(2)
 
-U,D,V = np.linalg.svd(W)
-vt=V.T
-#print(np.linalg.matrix_transpose(vt))
+# compute centroids
+centroids=[]
 
-#t=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+for camera in cameras:
+    sum=np.array([0.0,0.0])
+    for points in camera:
+        sum+=points
+    centroid=sum/len(camera)
+    centroids.append(centroid)
 
+for i in range(len(cameras)):
+    for j in range(len(cameras[i])):
+        cameras[i][j]-=centroids[i]
+
+# create the measurment matrix W
+# M[i][j] -> i indexes rows j indexes element of row M[:,j] indexes columns and returns row vector  M[:,[j]] indexes columns and returns colums vector
+W=np.empty((2*len(cameras),len(c1)))
+
+cameras_transpose=[]
+for camera in cameras:
+    cameras_transpose.append(camera.T)
+
+w=np.concatenate(cameras_transpose)
+
+# SVD of W
+U,D,V = np.linalg.svd(w,full_matrices=False)
+
+
+leters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+# Extract and print reconstruction of the points. The points are given in the same order as they appeared in the camera arrays
+points=[]
 for i in range(len(c1)):
-    p=[float(vt[i][0]),float(vt[i][1]),float(vt[i][2])]
-    s=f"X{i}={p}".replace("[","(").replace("]",")")
-    print(s)
+    x=(V).T[i]
+    x=[float(x[0]),float(x[1]),float(x[2])]
+    points.append(x)
+    text=str(x).replace("[","(").replace("]",")")
+    empty=""
+    print(f"{leters[i%26]+ ((str(int(i/26))) if len(c1) > 26 else empty) }={text}")
 
-u1=(U[:,0]*D[0])
-u2=U[:,1]*D[1]
-u3=U[:,2]*D[2]
+# Print the centroids (t_i)
+for i in range(len(centroids)):
+    print(f"t_{i}={centroids[i]}")
 
-MMatrix=np.array([u1,u2,u3]).transpose()
-
-
-for i in range(len(a)):
-    m=MMatrix[i*2:(i*2)+2,0:3]
-    print(f"M{i+1}={m}")
-
-for i in range(len(a)):
-    print(f"t{i+1}={c[i]}")
+# Extract and print camera matricies
+DU=U@np.diag(D)
+print(DU)
+mlist=[]
+for i in range(len(cameras)):
+    mi1=DU[2*i][0:3]
+    mi2=DU[2*i+1][0:3]
+    mi=np.array([mi1,mi2])
+    mlist.append(mi)
+    print(f"M{i}={mi}")
